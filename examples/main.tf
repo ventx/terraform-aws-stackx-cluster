eba683d4fe1a01db720c55ad3315a398d7a41a85
hansjoergventx <hansjoergventx>
2023-10-17 21:31:53 +0200

    docs: fix version in example

--- examples/main.tf
+++ examples/main.tf
@@ -1,7 +1,7 @@
 module "stackx-cluster" {
   source = "../"
 
-  cluster_version  = "1.28"
+  cluster_version  = "1.27"
   static_unique_id = "f2f6c971-6a3c-4d6e-9dca-7a3ba454d64d" # just random uuid generated for testing cut offs etc
   subnet_ids       = module.stackx-network.private_subnet_ids
 
